25932d770cdc38000645a54ef42dcbb085570293
Jaczel <jaczel@fastmail.com.au>
2020-03-03 18:50:57 +1100

    Updated to using tables. All values now update

--- bakerBuddy/bakerBuddyCtrl.js
+++ bakerBuddy/bakerBuddyCtrl.js
@@ -1,11 +1,41 @@
-app.controller('myCtrl', function($scope) {
+app.controller('bakerBuddyCtrl', function($scope) {
     $scope.flour=250;
     $scope.water=180;
     $scope.salt=0;
     $scope.yeast=0;
-    $scope.total=180/250*100;
-    $scope.updateTotal = function() {
-        var total = $scope.water / $scope.flour * 100;
-        $scope.total = Math.round(total);
+    $scope.hydration=$scope.water / $scope.flour * 100;
+    $scope.saltation=0;
+    $scope.yeastation=0;
+    $scope.updateHydration = function() {
+        var hydration = 0;
+        if ($scope.flour) {
+            hydration = $scope.water / $scope.flour * 100;
+            $scope.hydration = Math.round(hydration);
+        } else {
+            $scope.hydration = 100;
+        }
+    };
+    $scope.updateSalt = function() {
+        var saltation = 0;
+        if ($scope.flour) {
+            saltation = $scope.salt / $scope.flour * 100;
+            $scope.saltation = Math.round(saltation);
+        } else {
+            $scope.saltation = 0;
+        }
+    };
+    $scope.updateYeast = function() {
+        var yeastation = 0;
+        if ($scope.flour) {
+            yeastation = $scope.yeast / $scope.flour * 100;
+            $scope.yeastation = Math.round(yeastation);
+        } else {
+            $scope.yeastation = 0;
+        }
+    };
+    $scope.updatePercentages = function() {
+        $scope.updateHydration();
+        $scope.updateYeast();
+        $scope.updateSalt();
     };
 });
