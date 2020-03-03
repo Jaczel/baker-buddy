7c47a384ff22d288eec3c3682663840f13a006f4
Jaczel <jaczel@fastmail.com.au>
2020-03-03 21:27:20 +1100

    Updated values to floatswq

--- bakerBuddy/bakerBuddyCtrl.js
+++ bakerBuddy/bakerBuddyCtrl.js
@@ -1,41 +1,35 @@
 app.controller('bakerBuddyCtrl', function($scope) {
-    $scope.flour=250;
-    $scope.water=180;
-    $scope.salt=0;
-    $scope.yeast=0;
-    $scope.hydration=$scope.water / $scope.flour * 100;
-    $scope.saltation=0;
-    $scope.yeastation=0;
-    $scope.updateHydration = function() {
-        var hydration = 0;
-        if ($scope.flour) {
-            hydration = $scope.water / $scope.flour * 100;
-            $scope.hydration = Math.round(hydration);
-        } else {
-            $scope.hydration = 100;
-        }
-    };
-    $scope.updateSalt = function() {
-        var saltation = 0;
-        if ($scope.flour) {
-            saltation = $scope.salt / $scope.flour * 100;
-            $scope.saltation = Math.round(saltation);
-        } else {
-            $scope.saltation = 0;
-        }
-    };
-    $scope.updateYeast = function() {
-        var yeastation = 0;
-        if ($scope.flour) {
-            yeastation = $scope.yeast / $scope.flour * 100;
-            $scope.yeastation = Math.round(yeastation);
-        } else {
-            $scope.yeastation = 0;
-        }
-    };
-    $scope.updatePercentages = function() {
-        $scope.updateHydration();
-        $scope.updateYeast();
-        $scope.updateSalt();
-    };
+  $scope.flour=250.0;
+  $scope.water=180.0;
+  $scope.salt=5.0;
+  $scope.yeast=1.75;
+  $scope.hydration=$scope.water / $scope.flour * 100;
+  $scope.saltation=$scope.salt / $scope.flour * 100;
+  $scope.yeastation=Math.round($scope.yeast / $scope.flour * 100);
+  $scope.updateHydration = function() {
+    if ($scope.flour) {
+      $scope.hydration = Math.round($scope.water / $scope.flour * 100);
+    } else {
+      $scope.hydration = 100.0;
+    }
+  };
+  $scope.updateSalt = function() {
+    if ($scope.flour) {
+      $scope.saltation = Math.round($scope.salt / $scope.flour * 100);
+    } else {
+      $scope.saltation = 0.0;
+    }
+  };
+  $scope.updateYeast = function() {
+    if ($scope.flour) {
+      $scope.yeastation = Math.round($scope.yeast / $scope.flour * 100);
+    } else {
+      $scope.yeastation = 0.0;
+    }
+  };
+  $scope.updatePercentages = function() {
+    $scope.updateHydration();
+    $scope.updateYeast();
+    $scope.updateSalt();
+  };
 });
